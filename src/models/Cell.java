f06e479acdc10361a9b2dc713ea7e72a26684186
Likesh Kewat <leekesh1811@gmail.com>
2024-07-15 23:30:30 +0530

    Implemented the Cell model

---
+++ src/models/Cell.java
@@ -0,0 +1,46 @@
+package models;
+
+public class Cell {
+    private Player player;
+    private int row;
+    private int col;
+    private CellState cellState;
+
+    public Cell(int row, int col, CellState cellState) {
+        this.row = row;
+        this.col = col;
+        this.cellState = cellState;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public CellState getCellState() {
+        return cellState;
+    }
+
+    public void setCellState(CellState cellState) {
+        this.cellState = cellState;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public void setCol(int col) {
+        this.col = col;
+    }
+}
